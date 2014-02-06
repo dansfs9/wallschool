2ff5d2d7538e83472f88140e7baeef2e1e25e611
Daniel Araujo <dansfs9@gmail.com>
2014-02-06 10:40:50 -0200

    adicionei senha db

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'wordpress_bd');
 define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123123');
 
 /** nome do host do MySQL */
 define('DB_HOST', 'localhost');
